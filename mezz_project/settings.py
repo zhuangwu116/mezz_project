de8dd744a03a7101ab5739e2bcbc932ffcb350e1
zhuangwu <zhuangwu@zkhc.org>
2017-07-17 16:47:43 +0800

    ?

--- mezz_project/settings.py
+++ mezz_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conf.apps.ConfConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
