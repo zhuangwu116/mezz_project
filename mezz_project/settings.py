94d2cd740de2b2c21995803b68f2832faf8c1c02
zhuangwu <zhuangwu@zkhc.org>
2017-07-17 16:39:55 +0800

    ?

--- mezz_project/settings.py
+++ mezz_project/settings.py
@@ -72,14 +72,17 @@ WSGI_APPLICATION = 'mezz_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+try:
+    from local_settings import *
+except ImportError:
+    pass
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
